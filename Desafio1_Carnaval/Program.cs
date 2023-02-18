d6a1e259ca7b7243114648e68aba3daa23eed1eb
rafaelmoraism1 <rafaelmoraismartins@hotmail.com>
2023-02-17 22:00:27 -0300

    Carnaval

---
+++ Desafio1_Carnaval/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Desafio1_Carnaval
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto que eu irei contar quantos caracteres possui e quantas palavras:");
+
+            string? texto = Console.ReadLine();
+            int caracteres = texto.Length;
+            int palavras = texto.Split(' ').Length;
+
+            Console.WriteLine($"Seu texto possui {caracteres} caracteres e {palavras} palavras!");
+            Console.WriteLine("================================================================");
+
+        }
+    }
+}
