0d0d0eb06e4f71e99c15d0a135749da5293af125
rafaelmoraism1 <rafaelmoraismartins@hotmail.com>
2023-02-18 12:53:36 -0300

    Carnaval

---
+++ Desafio2_Carnaval/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Desafio2_Carnaval
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double calculoImc = 0;
+            Console.Clear();
+            Console.WriteLine("Vamos Calcular seu ICM!");
+            Console.WriteLine("Obs: Peso em Kg => Ex: 72 / Altura em metros => Ex: 1,70");
+            Console.WriteLine("========================================================");
+
+            Console.WriteLine("Digite seu PESO (kg): ");
+            double peso = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("Digite sua ALTURA (m): ");
+            double altura = double.Parse(Console.ReadLine());
+            calculoImc = peso / (altura * altura);
+            Console.WriteLine("-----------------------------------------------");
+
+            Console.WriteLine($"-> Seu IMC é {calculoImc.ToString("F2")} Kg/m2");
+
+            switch (calculoImc)
+            {
+                case < 16: Console.WriteLine("-> Magreza Grau III"); break;
+                case < 16.9: Console.WriteLine("-> Magreza Grau II"); break;
+                case < 18.4: Console.WriteLine("-> Magreza Grau I"); break;
+                case < 24.9: Console.WriteLine("-> Eutrofia"); break;
+                case < 29.9:
+                    Console.WriteLine("-> Sobrepeso");
+                    Console.WriteLine("-> Risco: Aumentado");
+                    break;
+                case < 34.9:
+                    Console.WriteLine("-> Obesidade Grau I");
+                    Console.WriteLine("-> Risco: Moderado");
+                    break;
+                case < 40:
+                    Console.WriteLine("-> Obesidade Grau II");
+                    Console.WriteLine("-> Risco: Grave");
+                    break;
+                default:
+                    Console.WriteLine("-> Obesidade Grau III");
+                    Console.WriteLine("-> Risco: Muito Grave");
+                    break;
+            }
+            Console.WriteLine("");
+        }
+
+    }
+
+}
