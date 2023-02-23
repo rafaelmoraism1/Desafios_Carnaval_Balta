ded3bac3386ff534f03342373ab86bcd4a208941
rafaelmoraism1 <rafaelmoraismartins@hotmail.com>
2023-02-23 13:07:12 -0300

    Carnaval

---
+++ Desafio7_Carnaval/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Desafio7_Carnaval
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine(" 1");
+            Console.WriteLine(" 1 1");
+            Console.WriteLine(" 1 1 2");
+            List<int> fibonacci = new List<int>() { 1, 2 };
+            string sequencia = " 1 1 2";
+
+            for (int valor = 1; valor != 35; valor++)
+            {
+                fibonacci.Add(fibonacci[valor] + fibonacci[valor - 1]);
+                sequencia += ($" {fibonacci[valor] + fibonacci[valor - 1]}");
+                Console.WriteLine($"{sequencia}");
+            }
+
+
+        }
+    }
+}
